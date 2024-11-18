ee168cad4c2b9ac7df60e70cf4d9e9d32fa99028
Alexey <bystrovalexey15@mail.ru>
2024-11-18 10:54:56 +0700

    homework 14.1 read from json

---
+++ src/utils.py
@@ -0,0 +1,28 @@
+import json
+import os
+from src.product import Product
+from src.category import Category
+
+
+def read_json(path: str) -> list[dict]:
+    full_path = os.path.abspath(path)
+    with open(full_path, 'r', encoding='UTF-8') as file:
+        data = json.load(file)
+    return data
+
+
+def data_from_json(data):
+    categories = []
+    for category in data:
+        products = []
+        for product in category['products']:
+            products.append(Product(**product))
+        category['products'] = products
+        categories.append(Category(**category))
+    return categories
+
+
+
+if __name__ == '__main__':
+    data = read_json('../data/products.json')
+    users_data = data_from_json(data)
