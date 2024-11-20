a2cb1dfa8ed98fa1f60141fdaa7a7d40345473df
Alexey <bystrovalexey15@mail.ru>
2024-11-20 15:22:52 +0700

    HW 14.2 add new method

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     name: str
     description: str
@@ -8,6 +11,23 @@ class Category:
     def __init__(self, name: str, description: str, products: list) -> None:
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    @property
+    def products(self):
+        return self.__products
+
+    def add_product(self, new_product: Product):
+        """Функция добавления продуктов"""
+        self.__products.append(new_product)
+        Category.product_count += 1
+
+    @property
+    def product_list(self):
+        """Функция вывода строки продуктов в нужном формате"""
+        products_str: str = ""
+        for product in self.__products:
+            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return products_str
--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ from src.category import Category
 
 def read_json(path: str) -> list[dict]:
     full_path = os.path.abspath(path)
-    with open(full_path, 'r', encoding='UTF-8') as file:
+    with open(full_path, "r", encoding="UTF-8") as file:
         data = json.load(file)
     return data
 
@@ -15,14 +15,13 @@ def data_from_json(data):
     categories = []
     for category in data:
         products = []
-        for product in category['products']:
+        for product in category["products"]:
             products.append(Product(**product))
-        category['products'] = products
+        category["products"] = products
         categories.append(Category(**category))
     return categories
 
 
-
-if __name__ == '__main__':
-    data = read_json('../data/products.json')
+if __name__ == "__main__":
+    data = read_json("../data/products.json")
     users_data = data_from_json(data)
