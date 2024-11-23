a029a7d15683f23a57f9d39fbd6b517d8e6eb78e
Alexey <bystrovalexey15@mail.ru>
2024-11-23 23:32:47 +0700

    tessts

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,6 +15,16 @@ def first_product():
 
 
 @pytest.fixture
+def second_product():
+    return Product(
+        "XIAOMI Ultra Pro",
+        "ядер - 6x(2.65 ГГц), 4 ГБ, 1 SIM, Super Retina XDR, 2436x1125, камера 12+12+12 Мп, NFC, 4G, GPS, 3190 мА*ч",
+        24000.0,
+        43,
+    )
+
+
+@pytest.fixture
 def first_category():
     return Category(
         "Смартфоны",
@@ -26,3 +36,11 @@ def first_category():
 @pytest.fixture
 def second_category():
     return Category("Верхняя одежда", "Одежда, для выхода на улицу", ["Куртка", "Шапка", "Кроссовки"])
+
+
+@pytest.fixture
+def third_category(first_product, second_product):
+    return Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [first_product, second_product])
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 def test_product_init(first_product):
     assert first_product.name == "Iphone 11 Pro"
     assert (
@@ -6,3 +9,29 @@ def test_product_init(first_product):
     )
     assert first_product.price == 24000.0
     assert first_product.quantity == 43
+
+
+def test_price_setter(capsys):
+    product = Product('Nokia 3300', 'Cool telephone', 0, 1)
+    product.price = 0
+    message = capsys.readouterr()
+    assert message.out.strip() == 'Цена не должна быть нулевая или отрицательная'
+    product.price = 2388.0
+    assert product.price == 2388.0
+
+
+new_product = Product.new_product(
+    {
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 5,
+    }
+)
+
+
+def test_new_product():
+    new_product.price = 0
+    assert new_product.price == 180000
+    new_product.price = 12000
+    assert new_product.price == 12000
