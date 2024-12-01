e305b82e99c9ea098f729b5bf2632cc14a121200
Alexey <bystrovalexey15@mail.ru>
2024-12-02 02:03:17 +0700

    tests

--- src/product.py
+++ src/product.py
@@ -59,6 +59,10 @@ class Smartphone(Product):
 
 
 class LawnGrass(Product):
+    country: str
+    germination_period: str
+    color: str
+
     def __init__(self, name, description, price, quantity, country, germination_period, color):
         super().__init__(name, description, price, quantity)
         self.country = country
